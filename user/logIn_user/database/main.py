7926f7ccf8b078add3267de5b336d6f0d8009306
XAMAH <iljakachalkin@yandex.ru>
2024-11-25 18:56:50 +0500

    .

--- user/logIn_user/database/main.py
+++ user/logIn_user/database/main.py
@@ -14,31 +14,52 @@ async def select_user(
         result: AutmeBaseModel = await session.execute(query)
         return result
 
-
 async def insert_user_new_token(
         data_user: model_autme,
         token: str,
         user_id: str
 ) -> bool:
     async with Session() as session:
-        new_device_user = DeviceBaseModel(
-            name=data_user.device.user_device,
-            ip_device=data_user.device.user_ip,
-        )
-        session.add(new_device_user)
-        await session.commit()
-        query = select(DeviceBaseModel).filter(DeviceBaseModel.name == data_user.device.user_device).order_by(DeviceBaseModel.id.desc())
-        result = await session.execute(query)
-        for current_device in result.scalars():
-            new_token_user = TokenBaseModel(
+        new_token_user = TokenBaseModel(
                 user_id=user_id,
                 token=token,
                 create_date=datetime.now(),
-                device=current_device.id
+                device=8
             )
-            session.add(new_token_user)
-            try:
-                await session.commit()
-            except Exception as e:
-                return False
-            return True
+        session.add(new_token_user)
+        try:
+            await session.commit()
+        except Exception as e:
+            return False
+        return True
+
+
+
+
+# async def insert_user_new_token(
+#         data_user: model_autme,
+#         token: str,
+#         user_id: str
+# ) -> bool:
+#     async with Session() as session:
+#         new_device_user = DeviceBaseModel(
+#             name=data_user.device.user_device,
+#             ip_device=data_user.device.user_ip,
+#         )
+#         session.add(new_device_user)
+#         await session.commit()
+#         query = select(DeviceBaseModel).filter(DeviceBaseModel.name == data_user.device.user_device).order_by(DeviceBaseModel.id.desc())
+#         result = await session.execute(query)
+#         for current_device in result.scalars():
+#             new_token_user = TokenBaseModel(
+#                 user_id=user_id,
+#                 token=token,
+#                 create_date=datetime.now(),
+#                 device=current_device.id
+#             )
+#             session.add(new_token_user)
+#             try:
+#                 await session.commit()
+#             except Exception as e:
+#                 return False
+#             return True
